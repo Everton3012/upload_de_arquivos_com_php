508af642db1a2e65054851f4593b5885eba61a3b
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-07 00:26:40 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+</head>
+<body>
+    <form enctype="multipart/form-data" action="">
+        <p><label for="">Selecione o arquivo</label>
+        <input type="file" name="" id=""></p>
+        <input type="submit" value="">
+    </form>
+</body>
+</html>
